5abef6fa44eaee105c9990fbb0d18536baabf6d9
santson <emer7681@gmail.com>
2025-11-23 15:10:58 -0300

    feat: Initialize multi-platform project structure for macOS, Windows, Linux, and Android.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,52 +1,44 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
+    id("com.android.application")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id "dev.flutter.flutter-gradle-plugin"
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.weatherpro.app"
+    namespace = "com.weatherpro.weatherpro"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        coreLibraryDesugaringEnabled true
     }
 
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.weatherpro.app"
+        applicationId = "com.weatherpro.weatherpro"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled true
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.debug
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
-    source '../..'
-}
-
-dependencies {
-    implementation "androidx.multidex:multidex:2.0.1"
-    implementation "androidx.core:core-ktx:1.17.0"
-    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.5'
+    source = "../.."
 }
---
+++ android/app/src/main/kotlin/com/weatherpro/weatherpro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.weatherpro.weatherpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
